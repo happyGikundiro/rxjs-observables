044b37892abaa4c6e2d5f8ae1f92d1e923d3a6c0
happyGikundiro <angehappy.gikundiro@amalitechtraining.org>
2024-09-05 15:10:51 +0200

    Implemented RxJS error handling and debugging

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { UserDisplayComponent } from './components/user-display/user-display.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    UserDisplayComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/model/model.ts
@@ -0,0 +1,6 @@
+export interface UserDetails {
+    id: number;
+    name: string;
+    description: string;
+    profile: string;
+  }
