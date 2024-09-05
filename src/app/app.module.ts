dcdce1519c297d4705cb6a96b07ed354fdeccc0c
happyGikundiro <angehappy.gikundiro@amalitechtraining.org>
2024-09-05 14:51:35 +0200

    Implemented RxJS search with debounce and data combination

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { SearchComponent } from './components/search/search.component';
+import { LoadingComponent } from './components/loading/loading.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    SearchComponent,
+    LoadingComponent,
   ],
   imports: [
     BrowserModule,
