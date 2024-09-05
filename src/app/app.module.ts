d64cde2b98cc02d28f1a971577ff17a5c5a69c57
happyGikundiro <angehappy.gikundiro@amalitechtraining.org>
2024-09-05 14:43:53 +0200

    Basic RxJs Observables

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SearchComponent } from './components/search/search.component';
 import { ReactiveFormsModule } from '@angular/forms';
-import { LoadingComponent } from './components/loading/loading.component';
-import { UserDisplayComponent } from './components/user-display/user-display.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SearchComponent,
-    LoadingComponent,
-    UserDisplayComponent,
   ],
   imports: [
     BrowserModule,
